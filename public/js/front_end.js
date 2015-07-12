9d27fb8a4e7f5431b214e31c7a10ff00f456f366
charu-dwivedi <charudwivedi455@gmail.com>
2015-07-11 20:41:17 -0700

    added front end

---
+++ public/js/front_end.js
@@ -0,0 +1,127 @@
+var user = angular.module('user', [])
+var admin = angular.module('admin', [])
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var user = angular.module('user', ['$scope', 'socket']);
+var admin = angular.module('admin', ['$scope', 'socket'])'
+
+user.factory('socket', ['$rootScope', function ($rootScope) {
+  var socket = io.connect();
+
+  return {
+    on: function (eventName, callback) {
+      function wrapper() {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      }
+
+      socket.on(eventName, wrapper);
+
+      return function () {
+        socket.removeListener(eventName, wrapper);
+      };
+    },
+
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if(callback) {
+            callback.apply(socket, args);
+          }
+        });
+      });
+    }
+  };
+}]);
+
+user.controller("userController", ['$scope', 'socket'. function($scope, $http) {
+    var th = this;
+    $scope.formData={};
+    $scope.loginData={};
+    $scope.image={};
+    $scope.helpData={};
+    $scope.weatherData=[];
+
+    io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+	    console.log('user disconnected');
+	});
+    });
+    
+    //insert socket here
+    io.on('Send weather', function(weather){
+	$scope.weatherData = weather;
+    });
+
+    $('help_form').onclick=function(){
+	socket.emit('Form back', $scope.formData);
+	$('#m').val('');
+	return false;
+    }
+}]);	    
+
+admin.factory('socket', ['$rootScope', function ($rootScope) {
+  var socket = io.connect();
+
+  return {
+    on: function (eventName, callback) {
+      function wrapper() {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      }
+
+      socket.on(eventName, wrapper);
+
+      return function () {
+        socket.removeListener(eventName, wrapper);
+      };
+    },
+
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if(callback) {
+            callback.apply(socket, args);
+          }
+        });
+      });
+    }
+  };
+}]);
+
+user.controller("userController", ['$scope', 'socket'. function($scope, $http) {
+    var th = this;
+    $scope.formData={};
+    $scope.loginData={};
+    $scope.image={};
+    $scope.helpData={};
+    $scope.weatherData=[];
+
+    io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+	    console.log('user disconnected');
+	});
+    });
+    
+    //insert socket here
+    io.on('Send weather', function(weather){
+	$scope.weatherData = weather;
+    });
+
+    $('help_form').onclick=function(){
+	socket.emit('Form back', $scope.formData);
+	$('#m').val('');
+	return false;
+    }
+}]);	    
+
