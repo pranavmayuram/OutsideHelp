e461125d0e235f2283a47daddf676af504e61e77
pranavmayuram <pranav.mayuram@couchbase.com>
2015-07-11 22:39:22 -0700

    routes with working socket.io

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,10 @@ var Session     = require("../models/sessionmodel");
 var uuid        = require("uuid");
 
 var appRouter = function(app) {
-
+	app.get('/', function(req, res) {
+            console.log("getting to index.html"); // load the single view file (angular will handle the page changes on the front-end)
+            res.sendfile('index.html');
+    });
 };
 
 module.exports = appRouter;
